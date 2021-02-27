ab4de2da344ea6fe1f14f0eb3170cebbbf7a9679
Daniel <daniel.serrano.gine@gmail.com>
2021-02-27 22:20:23 +0100

    info deleted

--- s3-trigger-slack-app/SendToSlack/slack.py
+++ s3-trigger-slack-app/SendToSlack/slack.py
@@ -5,7 +5,7 @@ from botocore.vendored import requests
 
 def sent_slack_message(data):
     JSON_HEADER = {'Content-Type': 'application/json'}
-    url = "https://hooks.slack.com/services/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"
+    url = AddUrlHook
     
     response = requests.post(url, data=json.dumps(data), headers=JSON_HEADER)
     
